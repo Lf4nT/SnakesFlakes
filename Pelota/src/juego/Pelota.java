8c35c953e305ab0156403b44785dc966e1323ee0
Javier Moran <javiermoran7@gmail.com>
2019-02-19 08:48:34 +0100

    Like a BOSS

--- Pelota/src/juego/Pelota.java
+++ Pelota/src/juego/Pelota.java
@@ -1,8 +1,13 @@
 package juego;
 
+import java.awt.AlphaComposite;
+import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Composite;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Stroke;
 
 @SuppressWarnings("unused")
 public class Pelota {
@@ -19,7 +24,8 @@ public class Pelota {
 	private int ymax;
 	private int rx;
 	private int ry;
-
+	private AlphaComposite composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1);
+	
 	public Pelota(Color color, int radio, int x, int y, double d, double v, Dimension dim) {
 		this.color = color;
 		this.radio = radio;
@@ -34,36 +40,48 @@ public class Pelota {
 		rx = dim.width + dim.width - diametro - diametro;
 		ry = dim.height + dim.height - diametro - diametro;
 	}
-
-	public void mover(long t) {
-		double dx = t * vx / 1000000000d;
-		double dy = t * vy / 1000000000d;
+	
+	public void mover(long lapso) {
+		double dx = lapso * vx / 1000000000d;
+		double dy = lapso * vy / 1000000000d;
 		x += dx;
 		y += dy;
 		if (x < 0) {
 			x = Math.abs(dx) - x;
 			vx *= -1;
-		} else if (x > xmax) {
+		}
+		else if (x > xmax) {
 			x = rx - x;
 			vx *= -1;
 		}
 		if (y < 0) {
 			y = Math.abs(dy) - y;
 			vy *= -1;
-		} else if (y > ymax) {
+		}
+		else if (y > ymax) {
 			y = ry - y;
 			vy *= -1;
 		}
 	}
-
+	
+	public boolean desaparecer(long lapso) {
+		double alpha = composite.getAlpha();
+		if (alpha > 0) {
+			alpha -= (double) lapso / 2000000000d;
+			if (alpha >= 0)
+				composite = composite.derive((float) alpha);
+		}
+		return alpha <= 0;
+	}
+	
 	public int getX() {
 		return (int) x + radio;
 	}
-
+	
 	public int getY() {
 		return (int) y + radio;
 	}
-
+	
 	public int getRadio() {
 		return radio;
 	}
@@ -74,5 +92,12 @@ public class Pelota {
 		g.setColor(Color.BLACK);
 		g.drawOval((int) x, (int) y, diametro, diametro);
 	}
-
+	
+	public void paintDesaparecer(Graphics g) {
+		Composite aux = ((Graphics2D) g).getComposite();
+		((Graphics2D) g).setComposite(composite);
+		paint(g);
+		((Graphics2D) g).setComposite(aux);
+	}
+	
 }
