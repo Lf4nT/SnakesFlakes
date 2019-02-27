d19529b2e34a2a857018e941563138a14ce2b145
Javier Moran <javiermoran7@gmail.com>
2019-02-27 11:41:47 +0100

    misclick

--- Juegos2D/src/snake/Main.java
+++ Juegos2D/src/snake/Main.java
@@ -10,7 +10,7 @@ public class Main{
 		JFrame frame = new JFrame("Pelota que Rebota");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		lienzo = new Lienzo(900, 650);
-		frame.add(lienzo);
+		frame.add(lienzo); 
 		frame.pack();
 		frame.setLocationRelativeTo(null);
 		SwingUtilities.invokeLater(new Runnable() {
